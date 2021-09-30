945eb72b7e7c56e0f6013c1c2e61f9f9429721f3
kevin fardeau <k.fardeau123@gmail.com>
2021-09-30 15:10:23 +0200

    ✅ Update DataBase

---
+++ migrations/Version20210930123824.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210930123824 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE message ADD user_id INT DEFAULT NULL, ADD snowtricks_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307FA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307FC38C303B FOREIGN KEY (snowtricks_id) REFERENCES snowtricks (id)');
+        $this->addSql('CREATE INDEX IDX_B6BD307FA76ED395 ON message (user_id)');
+        $this->addSql('CREATE INDEX IDX_B6BD307FC38C303B ON message (snowtricks_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307FA76ED395');
+        $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307FC38C303B');
+        $this->addSql('DROP INDEX IDX_B6BD307FA76ED395 ON message');
+        $this->addSql('DROP INDEX IDX_B6BD307FC38C303B ON message');
+        $this->addSql('ALTER TABLE message DROP user_id, DROP snowtricks_id');
+    }
+}
